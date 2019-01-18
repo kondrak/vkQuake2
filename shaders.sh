70005858e9dc6bc7f19cd07ddeb35aa9ad879f4a
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2019-01-19 00:27:49 +0100

    Added missing shebang.

--- shaders.sh
+++ shaders.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 glslangValidator --variable-name basic_vert_spv -V shaders/basic.vert -o ref_vk/spirv/basic_vert.c
 glslangValidator --variable-name basic_frag_spv -V shaders/basic.frag -o ref_vk/spirv/basic_frag.c
 glslangValidator --variable-name basic_color_quad_vert_spv -V shaders/basic_color_quad.vert -o ref_vk/spirv/basic_color_quad_vert.c
