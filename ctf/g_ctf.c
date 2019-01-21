0f714dcb7a29548fdda35d4b7d76904f014e4b46
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2019-01-21 19:18:31 +0100

    Fixed linker errors for CTF project.

--- ctf/g_ctf.c
+++ ctf/g_ctf.c
@@ -3215,10 +3215,10 @@ int CTFUpdateJoinMenu(edict_t *ent)
 	}
 
 	if (ctf_forcejoin->string && *ctf_forcejoin->string) {
-		if (stricmp(ctf_forcejoin->string, "red") == 0) {
+		if (Q_stricmp(ctf_forcejoin->string, "red") == 0) {
 			joinmenu[jmenu_blue].text = NULL;
 			joinmenu[jmenu_blue].SelectFunc = NULL;
-		} else if (stricmp(ctf_forcejoin->string, "blue") == 0) {
+		} else if (Q_stricmp(ctf_forcejoin->string, "blue") == 0) {
 			joinmenu[jmenu_red].text = NULL;
 			joinmenu[jmenu_red].SelectFunc = NULL;
 		}
