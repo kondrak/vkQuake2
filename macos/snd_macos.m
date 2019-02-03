1aae55016321deb632336394fef24760acaa2089
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2019-02-03 01:00:09 +0100

    Updated copyright notice.

--- macos/snd_macos.m
+++ macos/snd_macos.m
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 1997-2001 Id Software, Inc.
+ Copyright (C) 1997-2005 Id Software, Inc.
  Copyright (C) 2018-2019 Krzysztof Kondrak
  
  This program is free software; you can redistribute it and/or
