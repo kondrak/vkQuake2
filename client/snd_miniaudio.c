7a54e100e08696c8cf0fc3bc8e1f0f4298a3946d
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2019-07-09 01:03:42 +0200

    Added miniaudio library.

---
+++ client/snd_miniaudio.c
@@ -0,0 +1,9 @@
+#define DR_FLAC_IMPLEMENTATION
+#include "miniaudio/dr_flac.h"  /* Enables FLAC decoding. */
+#define DR_MP3_IMPLEMENTATION
+#include "miniaudio/dr_mp3.h"   /* Enables MP3 decoding. */
+#define DR_WAV_IMPLEMENTATION
+#include "miniaudio/dr_wav.h"   /* Enables WAV decoding. */
+
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
