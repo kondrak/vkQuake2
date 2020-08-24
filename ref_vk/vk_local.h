c8f713ee5b0ac2d417324b3b9ba965de8c66fe7a
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2020-08-24 21:01:17 +0200

    Bumped version.

--- ref_vk/vk_local.h
+++ ref_vk/vk_local.h
@@ -44,7 +44,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "qvk.h"
 
 #define	REF_VERSION	"Vulkan (vkQuake2 v"VKQUAKE2_VERSION")"
-#define VKQUAKE2_VERSION "1.5.2"
+#define VKQUAKE2_VERSION "1.5.3"
 
 // verify if VkResult is VK_SUCCESS
 #ifdef _DEBUG
