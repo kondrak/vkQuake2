beb6d93ef0692cb34c072fc0209d3bcfaeafb624
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2020-09-04 19:55:15 +0200

    No need to display weapon's HUD indicator when FOV is greater than 91.

--- zaero/p_hud.c
+++ zaero/p_hud.c
@@ -507,7 +507,7 @@ void G_SetStats (edict_t *ent)
 	//
 	if (ent->client->resp.helpchanged && (level.framenum&8) )
 		ent->client->ps.stats[STAT_HELPICON] = gi.imageindex ("i_help");
-	else if ( (ent->client->pers.hand == CENTER_HANDED || ent->client->ps.fov > 91)
+	else if ( (ent->client->pers.hand == CENTER_HANDED)
 		&& ent->client->pers.weapon)
 		ent->client->ps.stats[STAT_HELPICON] = gi.imageindex (ent->client->pers.weapon->icon);
 	else
