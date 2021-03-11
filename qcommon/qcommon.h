7506a7e2556ad3acbb759196b478e21a6795f0f6
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2021-03-11 20:04:15 +0100

    Rebuilt shaders using glslangValidator embedded with SDK 1.2.170.0.

--- qcommon/qcommon.h
+++ qcommon/qcommon.h
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define	VERSION		3.21
 
-#define VKQUAKE2_VERSION "1.5.6"
+#define VKQUAKE2_VERSION "1.5.7"
 
 #define	BASEDIRNAME	"baseq2"
 
