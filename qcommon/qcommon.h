bd2b347708c3cf94b6c1229108f6673a7bbe4c84
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2021-05-30 23:02:56 +0200

    Bumped version.

--- qcommon/qcommon.h
+++ qcommon/qcommon.h
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define	VERSION		3.21
 
-#define VKQUAKE2_VERSION "1.5.7"
+#define VKQUAKE2_VERSION "1.5.8"
 
 #define	BASEDIRNAME	"baseq2"
 
