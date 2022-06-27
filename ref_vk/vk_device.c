f42aab33d435cd77bfbf0ec0383c8d6c49028dc5
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2022-06-27 21:52:31 +0200

    Added Apple/MoltenVK to vendor list.

--- ref_vk/vk_device.c
+++ ref_vk/vk_device.c
@@ -267,6 +267,7 @@ static const char *vendorNameString(uint32_t vendorId)
 	{
 	case 0x1002: return "AMD";
 	case 0x1010: return "ImgTec";
+	case 0x106B: return "Apple";
 	case 0x10DE: return "NVIDIA";
 	case 0x13B5: return "ARM";
 	case 0x5143: return "Qualcomm";
